6461614b5e6085c50ede6cc4ceb29e2bfb428fd5
storeskhaliil <storeskhaliil@gmail.com>
2026-03-05 16:54:40 +0200

    Delete components directory

--- components/RestaurantHeader.jsx
+++
@@ -1,8 +0,0 @@
-export default function RestaurantHeader({ data }) {
-  return (
-    <div style={{ marginBottom: 40 }}>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-    </div>
-  );
-}
