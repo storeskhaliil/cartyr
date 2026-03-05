559e84d437112ac2c7e1905dbae8f10bdb58c894
storeskhaliil <storeskhaliil@gmail.com>
2026-03-05 22:11:55 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body className="bg-black text-white min-h-screen">
-        <nav className="p-4 border-b border-white/20 flex justify-between">
-          <a href="/feed" className="font-bold">Home</a>
-          <a href="/create-post">Create Post</a>
-          <a href="/login">Login</a>
-          <a href="/signup">Sign Up</a>
-        </nav>
-        <main className="p-4">{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
